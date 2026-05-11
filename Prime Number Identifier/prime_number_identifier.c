cc38d958e64c63a6d43b6ccae26f084cf83ecab3
Dibyajyoti Mishra <mishradibyajyoti011@gmail.com>
2026-05-11 21:13:38 +0530

    fixed minor issues

--- Prime Number Identifier/prime_number_identifier.c
+++ Prime Number Identifier/prime_number_identifier.c
@@ -10,7 +10,7 @@
 int main(){
     int isPrime = 1, n; // assuming as prime number
 
-    printf("Enter a num to check wheatear it is a prime no. or not: ");
+    printf("Enter a num to check whether it is a prime no. or not: ");
 
     if (scanf("%d", &n) != 1)
     {
@@ -19,12 +19,12 @@ int main(){
     }
     if (n<=1)
     {
-        printf("Please try with another number grater than 1");
+        printf("Please try with another number greater than 1");
         return 1;
     }
     
 
-    for (int i = 2; i * i < n; i++)
+    for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
         {
