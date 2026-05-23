9d64f56245ca586d8f35d370bad1cb148dfa9ab0
Dibyajyoti Mishra <mishradibyajyoti011@gmail.com>
2026-05-23 20:40:24 +0530

    Calculator is added
    Since I have created calculator multiple times in python, but this is my first calculator in C, I tried my best to handle errors. If you are seeing any errors in this program you can tell me.

---
+++ Calcultor/calculator.c
@@ -0,0 +1,58 @@
+/*
+ * Project: CLI Calculator in C
+ * Author: Raj (Dibyajyoti Mishra)
+ * Description: A reliable command-line calculator focused on memory safety and clean UI.
+ *
+ * Key Points:
+ * 1. Handles arithmetic operations (+, -, *, /) with float numbers.
+ * 2. Implements input validation on all inputs to prevent crashes.
+ * 3. Manages the stdin buffer space to avoid skipped char selections.
+ * 4. Checks for division by zero errors before execution.
+ * 5. Uses strict 2-decimal formatting (%.2f) for clean output.
+ */
+#include <stdio.h>
+
+int main(){
+    float num_1, num_2;
+    char operator;
+
+    printf("\nEnter the 1st num: ");
+    if(scanf("%f", &num_1) != 1){
+        printf("Invalid input, please enter a valid num\n");
+        return 1;
+    }
+
+    printf("\nEnter the operator (+ or - or  * or /): ");
+    if(scanf(" %c", &operator) != 1){
+        printf("Invalid input, please enter a valid operator\n");
+        return 1;
+    }
+
+    printf("\nEnter the 2nd num: ");
+    if(scanf("%f", &num_2) != 1){
+        printf("Invalid input, please enter a valid num\n");
+        return 1;
+    }
+
+    if (operator == '+'){
+        printf("%.2f %c %.2f = %.2f\n", num_1, operator, num_2, (num_1 + num_2));
+    }
+    
+    else if (operator == '-'){
+        printf("%.2f %c %.2f = %.2f\n", num_1, operator, num_2, (num_1 - num_2));
+    }
+
+    else if (operator == '*'){
+        printf("%.2f %c %.2f = %.2f\n", num_1, operator, num_2, (num_1 * num_2));
+    }
+
+    else if (operator == '/'){
+        printf("%.2f %c %.2f = %.2f\n", num_1, operator, num_2, (num_1 / num_2));
+    }
+    else{
+        printf("Somthing went wrong, you may entered some wrong input\n");
+    }
+
+
+    return 0;
+}
