e970b071025d08f3e2ed2cc43a4b36d4c994aa2c
Dibyajyoti Mishra <mishradibyajyoti011@gmail.com>
2026-06-12 11:15:09 +0530

    bug fixed

--- ATM Simulator/ATM_Simulator.c
+++ ATM Simulator/ATM_Simulator.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(){
-    int choise;
+int main()
+{
+    int choice;
     float balance = 100000.0, add_balance, withdraw;
     while (1)
     {
@@ -11,54 +12,59 @@ int main(){
         printf("3. Withdraw money\n");
         printf("4. Exit\n");
 
-        printf("\nEnter your choise: ");
-        if(scanf("%d", &choise) != 1){
-            printf("Invalid choise, plese select correct choise\n");
-            return 1;
-            break;
+        printf("\nEnter your choice: ");
+        if (scanf("%d", &choice) != 1)
+        {
+            printf("Invalid choice, please select correct choice\n");
+            return -1;
         }
 
-
-        if(choise == 1){
+        if (choice == 1)
+        {
             printf("Balance: %f\n", balance);
         }
-        else if (choise == 2)
+        else if (choice == 2)
         {
             printf("Your current balance: %f\n", balance);
             printf("Enter the amount to add: ");
-            if(scanf("%f", &add_balance) != 1){
-                printf("Invalid input, plese enter a valid num\n");
+            if (scanf("%f", &add_balance) != 1)
+            {
+                printf("Invalid input, please enter a valid num\n");
                 return 1;
             }
             balance += add_balance;
             printf("Success\nCurrent Balance: %f", balance);
-        
         }
-        else if (choise == 3)
+        else if (choice == 3)
         {
             printf("Your current balance: %f\n", balance);
-            if(scanf("%f", &withdraw) != 1){
-                printf("Invalid input, plese enter a valid num\n");
+            if (scanf("%f", &withdraw) != 1)
+            {
+                printf("Invalid input, please enter a valid num\n");
                 return 1;
             }
-            if(withdraw > balance){
+            if (withdraw > balance)
+            {
                 printf("You have not sufficient balance to withdraw\n");
                 return 1;
                 break;
             }
-            else{
+            else
+            {
                 balance -= withdraw;
                 printf("Success\nCurrent Balance: %f", balance);
             }
-
         }
-        else if (choise == 4)
+        else if (choice == 4)
         {
             printf("Successfully Exited\n");
             break;
         }
+        else
+        {
+            printf("You selected nothing, please try again\n");
+        }
     }
-        
-    
+
     return 0;
 }
