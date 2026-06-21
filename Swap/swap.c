a32e47bdbcf0b9236da09e1d72138d82c8eeb7e1
Dibyajyoti Mishra <mishradibyajyoti011@gmail.com>
2026-06-21 21:38:56 +0530

    swap using call by reference

---
+++ Swap/swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int*, int*);
+void swap(int* num1, int* num2){
+    int temp;
+    temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+
+}
+
+int main(){
+    int num_1 = 5, num_2 = 8;
+    
+    printf("The value of num 1 is %d and value of num 2 is %d\n", num_1, num_2);
+
+    swap(&num_1, &num_2);
+    printf("The value of num 1 is %d and value of num 2 is %d\n", num_1, num_2);
+    
+    return 0;
+}
